1d2c26375982ef45431f7497ddca5d7b3f1d4b37
hyn0027 <hongyn20@mails.tsinghua.edu.cn>
2023-06-07 10:46:16 +0800

    finished

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 fairseq-eval-lm data-bin/wikitext-103 \
-    --path checkpoints/PCA5/checkpoint_best.pt \
+    --path checkpoints/PCA1/checkpoint_best.pt \
     --batch-size 2 \
-    --tokens-per-sample 512 \
+    --tokens-per-sample 2048 \
     --context-window 400
---
+++ train_linformer6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm checkpoints/linformer6/*
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train --task language_modeling \
+    data-bin/wikitext-103 \
+    --save-dir checkpoints/linformer6 \
+    --low-rank Linformer --low-rank-scale 50 --low-rank-threshold 50 \
+    --arch transformerLowRank_lm --share-decoder-input-output-embed \
+    --log-interval 20 \
+    --dropout 0.1 \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0 \
+    --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
+    --tokens-per-sample 512 --sample-break-mode none \
+    --max-tokens 2048 --update-freq 2 \
+    --max-update 40000 \
+    --ddp-backend=legacy_ddp \
+    --max-epoch 40
